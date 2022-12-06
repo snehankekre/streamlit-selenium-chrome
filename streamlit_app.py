61eb38bf0af77ed61b317c577f396b823b678cac
Snehan Kekre <snehan.minerva@gmail.com>
2022-12-06 12:55:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,14 @@ import os, sys
 @st.experimental_singleton
 def install_chrome():
   os.system('sbase install chromedriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/chromedriver /home/appuser/venv/bin/chromedriver')
+#   os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/chromedriver /home/appuser/venv/bin/chromedriver')
 
 _ = install_chrome()
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 opts = Options()
-opts.add_argument("--headless")
-browser = webdriver.Chrome(options=opts)
+opts.headless = True
+browser = webdriver.Chrome(options=opts, executable_path="/home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/chromedriver")
 
 browser.get('http://example.com')
 st.write(browser.page_source)
