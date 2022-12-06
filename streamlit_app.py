fcd8dc09983d6f86171187a85f899a29ddbfe907
Snehan Kekre <snehan.minerva@gmail.com>
2022-12-06 13:19:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,30 +14,6 @@ options.add_argument('--disable-gpu')
 options.add_argument('--headless')
 
 driver = get_driver()
-
 driver.get('http://example.com')
-
-st.code(
-"""
-import streamlit as st
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
-@st.experimental_singleton
-def get_driver():
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-options = Options()
-options.add_argument('--disable-gpu')
-options.add_argument('--headless')
-
-driver = get_driver()
-
-driver.get('http://example.com')
-""", language="python"
-)
 
 st.code(driver.page_source)
